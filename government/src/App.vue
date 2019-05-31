562e55c3d2971a13edbb84d7f7e67223671b3b6d
Kait Tseng <jyp@livemail.tw>
2019-05-31 12:17:00 +0800

    Gogernment share

--- government/src/App.vue
+++ government/src/App.vue
@@ -1,70 +1,77 @@
 <template>
   <div id="app">
     <indicator></indicator>
-    <head-bar>
+    <head-bar head-color="#F7F7F7">
       <a href="https://udn.com/upf/newmedia/2019_data/recycle/" @click="onLinkItemClick('main')" aria-label="民眾回收一場空">民眾回收一場空</a>
-      <a href="#" aria-label="政府管理失能">政府管理失能</a>
-      <a href="https://udn.com/upf/newmedia/2019_data/recycle/reduction" @click.prevent="onLinkItemClick('2')" aria-label="環保不只一條路">環保不只一條路</a>
+      <a href="#" aria-label="政府燒錢有用嗎">政府燒錢有用嗎</a>
+      <a href="https://udn.com/upf/newmedia/2019_data/recycle/reduction" style="color: #464646; font-weight:bold;" @click="onLinkItemClick('2')" aria-label="環保不只一條路">環保不只一條路</a>
     </head-bar>
-    <cover src="static/government_cover_m.jpg" src-web="static/government_cover.jpg" position="leftTop">
-      <h1 class="title">政府管理失能<br><span v-if="!isWeb">&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;卻拿百姓開刀</h1>
-    </cover>
-    <content-container style="background:#191410;color:white;">
-      <p>民眾若未確實做好資源回收，現規定可依法開罰1200元到6000元不等，政府拿「開罰」做為督促分類的手段，但民眾乖乖分類後，官員卻沒有擔起該負的管理責任。經本報實地調查，被公告為
-        <dictionary title="應回收物">
-          <p style="margin:0;">餐盒、紙杯等皆屬環保署公告應回收項目中「廢紙容器類」，其餘廢紙容器包括紙餐具（含免洗餐具）、鋁箔包等。</p>
-        </dictionary>
-        的餐盒、紙杯，現在回收狀況一團亂，環保署卻一問三不知，嚴重失能。</p>
-      <br>
-      <p>詢問每年使用量，環保署推說不知道，也不清楚有多少回收物進了焚化爐；再問若全部餐盒、紙杯都落實回收，國內只有兩家再生處理廠夠嗎？環保署僅含糊稱「應該夠」，甚至明知回收業者因貪圖方便、拒收民眾分好的餐盒，官方也未積極處理。</p>
-      <br>
-      <p>從源頭的製造商、使用量，到後端的回收商、處理廠，環保署每一個環節都管理得不清不楚，只敢對老百姓開刀，這樣的管理能力，對得起配合政策、認真回收的民眾嗎？</p>
-      <span>FB </span>
-      <span>LINE</span>
-    </content-container>
-    <content-container>
-      <br>
-      <br>
-      <h3>問題一：<br>沒有使用量 政府靠感覺做事？</h3>
-      <br>
-      <p>先弄清楚民眾一年下來用了多少紙杯、紙盒，才能判斷現在的回收量占多少、回收成果如何，環保署手上的資料卻七零八落，沒有使用量數據、製造商也僅知合法且有加入公會的業者，但據記者追查，國內許多紙餐具都是透過家庭代工生產，這些都是政府沒有掌握的黑洞。</p>
-      <br>
-      <p>看守台灣協會協會秘書長謝和霖指出，許多外帶紙杯、紙餐盒都是地下生產，環保署至今難以掌握，依規定製造業者都需要繳交回收基金給環保署，但環署現在根本不可能要求這些地下業者負起回收責任。</p>
-    </content-container>
-    <parallaxBg src="static/government_image1_m.jpg" src-web="static/government_image1.jpg">
-    </parallaxBg>
-    <content-container style="padding-top:0;padding-bottom:0;">
-      <p class="image_description">紙餐盒許多都是家庭代工生產，製造量、使用量存有許多漏洞。</p>
-    </content-container>
-    <content-container style="min-height:100vh;display:flex;align-items:center;">
-      <p>不清楚使用量的另一個嚴重問題，是環保署根本無法評估國內的回收處理廠夠不夠、吃不吃得下所有餐盒。目前全台僅位於苗栗的連泰紙業、正隆紙業新竹竹北廠兩地可處理紙容器再生，兩個廠現在都擔心「供不應求」，環保署卻絲毫不緊張，稱「連泰紙業擴廠後應該就夠了」。</p>
-      <br>
-      <p>2018年紙容器回收量從2萬5千噸激增到4萬噸，主因雖是
-        <dictionary title="洋垃圾風暴">
-          <p style="margin:0;">2017年中國大陸頒布「禁廢令」，宣告2018年起不再接受包括廢紙、廢塑等24種廢棄物進口，國際廢紙價格因而大跌，國內紙廠開始收購便宜的海外廢紙，衝擊國內廢紙回收體系。</p>
-        </dictionary>
-        衝擊下，紙廠不再讓混著紙容器的紙磚進入紙廠，但目前每月僅能處理350噸的正隆紙業擔憂，如果餐盒、餐具等紙容器回收情形持續好轉，「我們的處理廠夠嗎？」政府大力推廣民眾做回收，回收廠恐怕大塞車。</p>
-      <!-- <fb-comment href="https://udn.com/upf/newmedia/2019_data/DUI_victim_stories/"></fb-comment> -->
-    </content-container>
-    <parallaxBg src="static/government_image2_m.jpg" src-web="static/government_image2.jpg">
-    </parallaxBg>
-    <content-container style="padding-top:0;padding-bottom:0;">
-      <p class="image_description">正隆紙業的處理量每月最多只能處理350公噸紙容器。</p>
-    </content-container>
-    <content-container style="min-height:100vh;display:flex;align-items:center;">
-      <p>連泰紙業目前每月可以處理5000噸紙容器，一年下來6萬噸，看似還有空間，但連泰紙業已著手擴廠，預計每月處理量將增長至12000噸，提早做準備。</p>
-      <br>
-      <p>連泰紙業經理連大鈞指出，2018年8月以前，每月平均僅有2000多噸的回收量，但8月開始回收量激增至近5000噸，達到連泰的產能極限，2019年才再投資新的設備，但近幾個月又發現回收量開始停滯，「有點沒信心」，連泰也曾向環保署回收基金管會詢問過全台紙容器應回收量究竟有多少，都沒得到回覆。</p>
-      <br>
-      <br>
-      <br>
-      <h3>問題二：<br>數字一團亂 補貼制度失能</h3>
-      <br>
-      <p>源頭數字混亂，讓後端回收補貼費率也一團亂。國內回收體系價格調節，都仰賴環保署資源回收基金管理會（簡稱回收基管會），基管會向生產商收取回收基金，再設定費率補貼回收業者及處理廠，但走訪第一線回收場，環保署對紙容器的補貼沒有發揮作用，許多回收場還是不想好好做。</p>
-    </content-container>
-    <parallaxBg src="static/government_image3-1_m.jpg" src-web="static/government_image3-1.jpg">
-    </parallaxBg>
-    <cover src="static/government_image3-2_m.jpg" src-web="static/government_image3-2.jpg"></cover>
+    <div ref="story0" id="story0">
+      <cover src="static/government_cover_m.jpg" src-web="static/government_cover.jpg" position="leftTop">
+        <h1 class="title">政府管理失能<br><span v-if="!isWeb">&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;卻拿百姓開刀</h1>
+      </cover>
+      <content-container style="background:#191410;color:white;">
+        <p>民眾若未確實做好資源回收，現規定可依法開罰1200元到6000元不等，政府拿「開罰」做為督促分類的手段，但民眾乖乖分類後，官員卻沒有擔起該負的管理責任。經本報實地調查，被公告為
+          <dictionary title="應回收物">
+            <p style="margin:0;">餐盒、紙杯等皆屬環保署公告應回收項目中「廢紙容器類」，其餘廢紙容器包括紙餐具（含免洗餐具）、鋁箔包等。</p>
+          </dictionary>
+          的餐盒、紙杯，現在回收狀況一團亂，環保署卻一問三不知，嚴重失能。</p>
+        <br>
+        <p>詢問每年使用量，環保署推說不知道，也不清楚有多少回收物進了焚化爐；再問若全部餐盒、紙杯都落實回收，國內只有兩家再生處理廠夠嗎？環保署僅含糊稱「應該夠」，甚至明知回收業者因貪圖方便、拒收民眾分好的餐盒，官方也未積極處理。</p>
+        <br>
+        <p>從源頭的製造商、使用量，到後端的回收商、處理廠，環保署每一個環節都管理得不清不楚，只敢對老百姓開刀，這樣的管理能力，對得起配合政策、認真回收的民眾嗎？</p>
+        <ShareButton />
+      </content-container>
+    </div>
+    <div ref="story1" id="story1">
+      <content-container>
+        <br>
+        <br>
+        <h3>問題一：<br>沒有使用量 政府靠感覺做事？</h3>
+        <br>
+        <p>先弄清楚民眾一年下來用了多少紙杯、紙盒，才能判斷現在的回收量占多少、回收成果如何，環保署手上的資料卻七零八落，沒有使用量數據、製造商也僅知合法且有加入公會的業者，但據記者追查，國內許多紙餐具都是透過家庭代工生產，這些都是政府沒有掌握的黑洞。</p>
+        <br>
+        <p>看守台灣協會協會秘書長謝和霖指出，許多外帶紙杯、紙餐盒都是地下生產，環保署至今難以掌握，依規定製造業者都需要繳交回收基金給環保署，但環署現在根本不可能要求這些地下業者負起回收責任。</p>
+      </content-container>
+      <parallaxBg src="static/government_image1_m.jpg" src-web="static/government_image1.jpg">
+      </parallaxBg>
+      <content-container style="padding-top:0;padding-bottom:0;">
+        <p class="image_description">紙餐盒許多都是家庭代工生產，製造量、使用量存有許多漏洞。</p>
+      </content-container>
+      <content-container style="min-height:100vh;display:flex;align-items:center;">
+        <p>不清楚使用量的另一個嚴重問題，是環保署根本無法評估國內的回收處理廠夠不夠、吃不吃得下所有餐盒。目前全台僅位於苗栗的連泰紙業、正隆紙業新竹竹北廠兩地可處理紙容器再生，兩個廠現在都擔心「供不應求」，環保署卻絲毫不緊張，稱「連泰紙業擴廠後應該就夠了」。</p>
+        <br>
+        <p>2018年紙容器回收量從2萬5千噸激增到4萬噸，主因雖是
+          <dictionary title="洋垃圾風暴">
+            <p style="margin:0;">2017年中國大陸頒布「禁廢令」，宣告2018年起不再接受包括廢紙、廢塑等24種廢棄物進口，國際廢紙價格因而大跌，國內紙廠開始收購便宜的海外廢紙，衝擊國內廢紙回收體系。</p>
+          </dictionary>
+          衝擊下，紙廠不再讓混著紙容器的紙磚進入紙廠，但目前每月僅能處理350噸的正隆紙業擔憂，如果餐盒、餐具等紙容器回收情形持續好轉，「我們的處理廠夠嗎？」政府大力推廣民眾做回收，回收廠恐怕大塞車。</p>
+        <!-- <fb-comment href="https://udn.com/upf/newmedia/2019_data/DUI_victim_stories/"></fb-comment> -->
+      </content-container>
+      <parallaxBg src="static/government_image2_m.jpg" src-web="static/government_image2.jpg">
+      </parallaxBg>
+      <content-container style="padding-top:0;padding-bottom:0;">
+        <p class="image_description">正隆紙業的處理量每月最多只能處理350公噸紙容器。</p>
+      </content-container>
+      <content-container style="display:flex;align-items:center;">
+        <p>連泰紙業目前每月可以處理5000噸紙容器，一年下來6萬噸，看似還有空間，但連泰紙業已著手擴廠，預計每月處理量將增長至12000噸，提早做準備。</p>
+        <br>
+        <p>連泰紙業經理連大鈞指出，2018年8月以前，每月平均僅有2000多噸的回收量，但8月開始回收量激增至近5000噸，達到連泰的產能極限，2019年才再投資新的設備，但近幾個月又發現回收量開始停滯，「有點沒信心」，連泰也曾向環保署回收基金管會詢問過全台紙容器應回收量究竟有多少，都沒得到回覆。</p>
+        <br>
+        <br>
+        <br>
+      </content-container>
+    </div>
+    <div ref="story2" id="story2">
+      <content-container style="display:flex;align-items:center;">
+        <h3>問題二：<br>數字一團亂 補貼制度失能</h3>
+        <br>
+        <p>源頭數字混亂，讓後端回收補貼費率也一團亂。國內回收體系價格調節，都仰賴環保署資源回收基金管理會（簡稱回收基管會），基管會向生產商收取回收基金，再設定費率補貼回收業者及處理廠，但走訪第一線回收場，環保署對紙容器的補貼沒有發揮作用，許多回收場還是不想好好做。</p>
+      </content-container>
+      <parallaxBg src="static/government_image3-1_m.jpg" src-web="static/government_image3-1.jpg">
+      </parallaxBg>
+      <cover src="static/government_image3-2_m.jpg" src-web="static/government_image3-2.jpg"></cover>
+    </div>
     <content-container style="padding-top:0;">
       <p class="image_description">2018年紙容器回收量在洋垃圾衝擊下明顯成長，但國內長期存在的紙容器回收問題仍未改善。圖為進入紙容器處理廠、連泰紙業的紙磚。</p>
       <br>
@@ -84,82 +91,92 @@
       <br>
       <br>
       <br>
-      <h3>問題三：<br>回收半調子 百姓受害</h3>
-      <br>
-      <p>面對每天幾乎都會用到的紙餐盒、咖啡杯，多數民眾最深的困擾是：「到底能不能回收啊？是算紙類嗎？」被分類搞得霧煞煞。但這並非民眾的問題，而是政府、業者混淆了民眾。</p>
-      <br>
-      <p>從一張公文就能看見回收體系的崩壞。雙北的公寓大廈出現公文，指因紙廠改變政策拒收紙杯、免洗餐具、鋁箔包，要住戶直接當垃圾丟棄；部分社區大樓收集好餐盒出售給回收場，卻被業者直接挑出來，挑明跟住戶說「這不回收」。</p>
-      <br>
-      <p>新北一間企業員工餐廳雖設置空間讓顧客回收紙餐具，整理好後卻被清運業者打槍「不能收」，總務單位更直言「這不能賣錢，不能回收」，長期下來民眾根本搞不清楚紙餐盒要丟哪，若被清潔隊員抓到只能喊苦。</p>
-      <br>
-      <column-one src="static/government_image4.jpg" src-web="static/government_image4.jpg" description="各地破袋稽查愈來愈嚴格，但許多民眾至今搞不清楚紙餐盒到底算不算回收物。" href="https://udn.com/upf/newmedia/2019_data/recycle/government"></column-one>
-      <br>
-      <p>將此亂象詢問環保署，環保署表示知情，但坦言許多回收商長期累積下來的習慣改不掉，還是將紙容器跟紙類混在一起送入紙廠，一般紙廠其實無法消化紙容器，一樣變垃圾。很多回收業者也都知道有連泰等處理廠在收，「只是不想改變既有回收模式」，回收是自由市場，環署只能盡量輔導鼓勵。</p>
-      <br>
-      <br>
-      <br>
-      <h3>解方：政府的角色在哪裡？</h3>
-      <br>
-      <p>民眾每年平均使用超過200個的紙餐盒，現在的回收體系卻漏洞一堆，環保署既然以開罰這樣具強制性的手段要求民眾做分類，就必須負起責任，對種種亂象不能只以一句「回收是自由市場」輕輕帶過。</p>
-      <br>
-      <p>第一，環保署需加強源頭管理力道，搞清楚有多少製造商、到底用了幾個，弄清楚源頭數量，才能仔細檢視回收現況，繼而評估現行補貼政策是否需調整。</p>
-      <br>
-      <p>第二，紙餐盒回收處理面臨的困境在於供需意願落差大，連泰紙業等處理廠因補貼誘因強，再利用後又可賣錢，積極擴廠想收更多，但回收商卻因分類、清洗、運輸等瓶頸，回收意願低落，反映出補貼機制明顯失衡，環保署需要檢討。</p>
-      <br>
-      <p>第三，許多民眾並非不願意配合分類，而是被政府、業者弄得霧煞煞，加上紙餐盒等紙容器因需清洗，比紙類等回收麻煩，不好處理就直接丟進垃圾桶。謝和霖建議，環署的補貼機制可延伸到民眾，當民眾分類有得到鼓勵，「大家就會願意去做。」</p>
-      <br>
-      <p>以德國為例，鋁罐、玻璃啤酒瓶、礦泉水瓶等只要投入專門機器，就能兌換現金抵消費；提出「零垃圾宣言」的日本上勝町發現牛奶包裝盒等零碎紙類難回收，設計出集點制度讓民眾兌換生活用品，都是可參考的做法。</p>
-      <br>
-      <p>除了加強回收管理，「減少使用」才是真正從源頭解決的方法，除了鼓勵民眾自己攜帶環保餐具，民間已蓬勃發展出許多創意減量方法，都值得政府參考。</p>
-      <br>
-    </content-container>
-    <div class="question-btns">
-      <div class="question-btn-layout">
-        <div class="question-btn"><!--
-        --><a @click="onQuestionBtnClick(1)" href="https://udn.com/upf/newmedia/2019_data/recycle/reduction/" class="frame-31__complain-btn" title="" target="_blank">減量好方法看這裡</a><!--
-        --><a @click="onQuestionBtnClick(2)" href="https://udn.com/upf/newmedia/2019_data/recycle/" class="frame-31__complain-btn" title="" target="_blank">餐盒回收為何成為一場空？</a><!--
-      --></div>
+      <div ref="story3" id="story3">
+        <h3>問題三：<br>回收半調子 百姓受害</h3>
+        <br>
+        <p>面對每天幾乎都會用到的紙餐盒、咖啡杯，多數民眾最深的困擾是：「到底能不能回收啊？是算紙類嗎？」被分類搞得霧煞煞。但這並非民眾的問題，而是政府、業者混淆了民眾。</p>
+        <br>
+        <p>從一張公文就能看見回收體系的崩壞。雙北的公寓大廈出現公文，指因紙廠改變政策拒收紙杯、免洗餐具、鋁箔包，要住戶直接當垃圾丟棄；部分社區大樓收集好餐盒出售給回收場，卻被業者直接挑出來，挑明跟住戶說「這不回收」。</p>
+        <br>
+        <p>新北一間企業員工餐廳雖設置空間讓顧客回收紙餐具，整理好後卻被清運業者打槍「不能收」，總務單位更直言「這不能賣錢，不能回收」，長期下來民眾根本搞不清楚紙餐盒要丟哪，若被清潔隊員抓到只能喊苦。</p>
+        <br>
+        <column-one src="static/government_image4.jpg" src-web="static/government_image4.jpg" description="各地破袋稽查愈來愈嚴格，但許多民眾至今搞不清楚紙餐盒到底算不算回收物。" href="https://udn.com/upf/newmedia/2019_data/recycle/government"></column-one>
+        <br>
       </div>
+        <p>將此亂象詢問環保署，環保署表示知情，但坦言許多回收商長期累積下來的習慣改不掉，還是將紙容器跟紙類混在一起送入紙廠，一般紙廠其實無法消化紙容器，一樣變垃圾。很多回收業者也都知道有連泰等處理廠在收，「只是不想改變既有回收模式」，回收是自由市場，環署只能盡量輔導鼓勵。</p>
+        <br>
+        <br>
+        <br>
+      <div ref="story4" id="story4">
+        <h3>解方：政府的角色在哪裡？</h3>
+        <br>
+        <p>民眾每年平均使用超過200個的紙餐盒，現在的回收體系卻漏洞一堆，環保署既然以開罰這樣具強制性的手段要求民眾做分類，就必須負起責任，對種種亂象不能只以一句「回收是自由市場」輕輕帶過。</p>
+        <br>
+        <p>第一，環保署需加強源頭管理力道，搞清楚有多少製造商、到底用了幾個，弄清楚源頭數量，才能仔細檢視回收現況，繼而評估現行補貼政策是否需調整。</p>
+        <br>
+        <p>第二，紙餐盒回收處理面臨的困境在於供需意願落差大，連泰紙業等處理廠因補貼誘因強，再利用後又可賣錢，積極擴廠想收更多，但回收商卻因分類、清洗、運輸等瓶頸，回收意願低落，反映出補貼機制明顯失衡，環保署需要檢討。</p>
+        <br>
+        <p>第三，許多民眾並非不願意配合分類，而是被政府、業者弄得霧煞煞，加上紙餐盒等紙容器因需清洗，比紙類等回收麻煩，不好處理就直接丟進垃圾桶。謝和霖建議，環署的補貼機制可延伸到民眾，當民眾分類有得到鼓勵，「大家就會願意去做。」</p>
+        <br>
+        <p>以德國為例，鋁罐、玻璃啤酒瓶、礦泉水瓶等只要投入專門機器，就能兌換現金抵消費；提出「零垃圾宣言」的日本上勝町發現牛奶包裝盒等零碎紙類難回收，設計出集點制度讓民眾兌換生活用品，都是可參考的做法。</p>
+        <br>
+        <p>除了加強回收管理，「減少使用」才是真正從源頭解決的方法，除了鼓勵民眾自己攜帶環保餐具，民間已蓬勃發展出許多創意減量方法，都值得政府參考。</p>
+        <br>
+      </div>
+    </content-container>
+    <div ref="story5" id="story5">
+      <div class="question-btns">
+        <div class="question-btn-layout">
+          <div class="question-btn"><!--
+          --><a @click="onQuestionBtnClick(1)" href="https://udn.com/upf/newmedia/2019_data/recycle/reduction/" class="frame-31__complain-btn" title="" target="_blank">減量好方法看這裡</a><!--
+          --><a @click="onQuestionBtnClick(2)" href="https://udn.com/upf/newmedia/2019_data/recycle/" class="frame-31__complain-btn" title="" target="_blank">餐盒回收為何成為一場空？</a><!--
+        --></div>
+        </div>
+      </div>
+      <content-container style="background:#30343f;color:#e4e4e4;">
+        <hr>
+        <br>
+        <br>
+        <br>
+        <br>
+        <share href="https://udn.com/upf/newmedia/2019_data/recycle/government/"></share>
+        <br>
+        <br>
+        <br>
+        <editor style="color:#e4e4e4;">
+          <div>採訪團隊：鄭朝陽、郭政芬、張裕珍、林敬家、張雅婷、陳斯穎、許政榆、賴香珊、范榮達、魏翊庭</div>
+          <div>製作人：洪欣慈</div>
+          <div>影像：黃仲裕、郭政芬、賴香珊、報系資料照</div>
+          <div>視覺設計：張心慈</div>
+          <div>網頁製作：謝化挺</div>
+          <div>監製：林秀姿、潘如瑩、董谷音</div>
+          <div>2019.06.05</div>
+        </editor>
+        <br>
+        <logo use-ubrand="no" use-vision='no'></logo>
+        <br>
+        <br>
+        <question href="https://www.surveycake.com/s/KpQKN" text="填寫閱讀體驗問卷"></question>
+        <br>
+        <br>
+        <br>
+        <relate
+          text1="延宕限塑、回收機制差 監院糾正環署" href1="https://udn.com/news/story/7266/3806198" img1="static/government_read1.jpg"
+          text2="強化回收再利用率 環保署長：調整補貼制度" href2="https://udn.com/news/story/7314/3617009" img2="static/government_read2.jpg"
+          text3="塑膠、膠膜回收 研議從原料課費" href3="https://udn.com/news/story/7266/3796119" img3="static/government_read3.jpg"
+          text4="紙容器塑膠淋膜革命 大學生組隊赴美參賽" href4="https://udn.com/news/story/6928/3425491" img4="static/government_read4.jpg">
+        </relate>
+      </content-container>
+      <content-container>
+        <fb-comment href="https://udn.com/upf/newmedia/2019_data/recycle/government"></fb-comment>
+      </content-container>
+      <foot></foot>
     </div>
-    <content-container style="background:#30343f;color:#e4e4e4;">
-      <hr>
-      <br>
-      <br>
-      <br>
-      <br>
-      <share href="https://udn.com/upf/newmedia/2019_data/recycle/government/"></share>
-      <br>
-      <br>
-      <br>
-      <editor style="color:#e4e4e4;">
-        <div>採訪團隊：鄭朝陽、郭政芬、張裕珍、林敬家、張雅婷、陳斯穎、許政榆、賴香珊、范榮達、魏翊庭</div>
-        <div>製作人：洪欣慈</div>
-        <div>影像：黃仲裕、郭政芬、賴香珊、報系資料照</div>
-        <div>視覺設計：張心慈</div>
-        <div>網頁製作：曾雅珮、謝化挺</div>
-        <div>監製：林秀姿、潘如瑩、董谷音</div>
-        <div>2019.06.05</div>
-      </editor>
-      <br>
-      <logo use-ubrand="no" use-vision='no'></logo>
-      <br>
-      <br>
-      <question href="https://www.surveycake.com/s/KpQKN" text="填寫閱讀體驗問卷"></question>
-      <br>
-      <br>
-      <br>
-      <relate
-        text1="延宕限塑、回收機制差 監院糾正環署" href1="https://udn.com/news/story/7266/3806198" img1="static/government_read1.jpg"
-        text2="強化回收再利用率 環保署長：調整補貼制度" href2="https://udn.com/news/story/7314/3617009" img2="static/government_read2.jpg"
-        text3="塑膠、膠膜回收 研議從原料課費" href3="https://udn.com/news/story/7266/3796119" img3="static/government_read3.jpg"
-        text4="紙容器塑膠淋膜革命 大學生組隊赴美參賽" href4="https://udn.com/news/story/6928/3425491" img4="static/government_read4.jpg">
-      </relate>
-    </content-container>
-    <content-container>
-      <fb-comment href="https://udn.com/upf/newmedia/2019_data/recycle/government"></fb-comment>
-    </content-container>
-    <foot></foot>
     <back-top></back-top>
+    <ScrollSpy
+      :focus-story="focusStory"
+      @jump="onScrollSpyJump"
+      :active= "isScrollSpyShow" />
   </div>
 </template>
 
@@ -187,9 +204,23 @@
   import Share from './components/Share.vue'
   import Youtube from './components/Youtube.vue'
   import Dictionary from './components/Dictionary.vue'
+  import ScrollSpy from './components/ScrollSpy'
+  import ShareButton from './components/ShareButton'
 
   import srcRWD from "./mixin/srcRWD.js"
 
+const THRESHOLD = [
+  0.05, 0.1,
+  0.15, 0.2,
+  0.25, 0.3,
+  0.35, 0.4,
+  0.45, 0.5,
+  0.55, 0.6,
+  0.65, 0.7,
+  0.75, 0.8,
+  0.85, 0.9,
+  0.95, 1.0
+]
 
 export default {
   name: 'app',
@@ -217,14 +248,26 @@ export default {
     ParallaxBg,
     Share,
     Youtube,
-    Dictionary
+    Dictionary,
+    ScrollSpy,
+    ShareButton
   },
   data() {
     return {
       isWeb: false,
+      isScrollSpyJumping: false,
+      focusStory: null,
+      observer: null,
+      isScrollSpyShow: false
     }
   },
-
+  computed: {
+    className () {
+      return {
+        [this.focusStory]: !!this.focusStory
+      }
+    }
+  },
   methods: {
     onQuestionBtnClick (id) {
       ga('newmedia.send', {
@@ -241,7 +284,31 @@ export default {
         "eventAction": "click",
         "eventLabel": `page1to${id}`
       })
-    }
+    },
+    onScrollSpyJump (event, id) {
+      $('html, body').animate({
+          scrollTop: $(`#${event.storyId}`).offset().top
+      }, 1000);
+    },
+    onStoryIntersection (entries) {
+      let maxStoryRatio = 0
+      let focusStory = null
+      entries.forEach((entry) => {
+        let { target, intersectionRatio } = entry
+        let ratio = Math.round(intersectionRatio * 1000) / 100
+        if (ratio > 0 && ratio >= maxStoryRatio) {
+          maxStoryRatio = ratio
+          focusStory = target.id
+        }
+        if (parseInt(target.id.split('story')[1]) > 0 && parseInt(target.id.split('story')[1]) < 5) {
+          this.isScrollSpyShow = true
+        } else {
+          this.isScrollSpyShow = false
+        }
+        console.log(parseInt(target.id.split('story')[1]))
+      })
+      this.focusStory = focusStory
+    },
   },
   mounted(){
     if(window.innerWidth>=768){
@@ -249,6 +316,28 @@ export default {
     }else{
       this.isWeb = false;
     }
+
+    this.observer = new IntersectionObserver(this.onStoryIntersection, {
+      root: null,
+      rootMargin: '0px 0px 0px 0px',
+      threshold: THRESHOLD
+    })
+    let stories = [
+      this.$refs.story0,
+      this.$refs.story1,
+      this.$refs.story2,
+      this.$refs.story3,
+      this.$refs.story4,
+      this.$refs.story5
+    ]
+    stories.forEach((story) => {
+      this.observer.observe(story)
+    })
+    // if (this.observer.length > 0) {
+    //   this.isScrollSpyShow = true
+    // } else {
+    //   this.isScrollSpyShow = false
+    // }
   }
 }
 </script>
